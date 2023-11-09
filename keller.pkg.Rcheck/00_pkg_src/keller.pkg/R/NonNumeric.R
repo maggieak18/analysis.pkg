190e1a8d1ae37fe04769f7649b241fd226662a55
Maggie Keller <magdalene.keller@selu.edu>
2023-11-09 20:00:00 +0000

    finished functions

--- keller.pkg.Rcheck/00_pkg_src/keller.pkg/R/NonNumeric.R
+++ keller.pkg.Rcheck/00_pkg_src/keller.pkg/R/NonNumeric.R
@@ -1,12 +1,32 @@
-#This function is a way to clean data by checking to see if any non-numeric data
-#is in the columns where it shouldnt be.
+#'Check if column contents are numeric
+#'
+#'Clean data by checking to see if any non-numeric data is in a column where it shouldn't be.
+#'
+#'@param column the column you wish to clean. Must be selected out of data set and saved to a variable.
+#'@return if the column is numeric, the column will be returned
+#'
+#'@export
 
-```{r}
+
 non_numeric <- function(column){
-  if(is.numeric(column)==TRUE){
+ if (length((print(select_if(column, is.numeric)))== 1)){
     return(column)
   } else {
     print("Non-numeric data in column")
   }
 }
-```
+
+
+#examples
+#
+#frog_number <- frog_data %>% 
+#  + select(frog_number)
+#
+#non_numeric(frog_number)
+#
+#
+#ex_group <- frog_data %>% 
+#  + select(experimental_group)
+#
+#non_numeric(ex_group)
+#
